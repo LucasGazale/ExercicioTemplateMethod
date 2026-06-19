253e3bac0aabcce06f7cc08c286ce8515b563d43
LucasGazale <lucas.gazale@unifesp.br>
2026-06-19 14:54:44 -0300

    Implement sorting of words by last letter
    Added a comparer to sort words by their last letter.

--- Program.cs
+++ Program.cs
@@ -1,4 +1,15 @@
 using System;
+using System.Collections.Generic;
+
+class ComparaUltimaLetra : IComparer<string>
+{
+    public int Compare(string palavra1, string palavra2)
+    {
+        char ultimaLetra1 = palavra1[palavra1.Length - 1];
+        char ultimaLetra2 = palavra2[palavra2.Length - 1];
+        return ultimaLetra1.CompareTo(ultimaLetra2);
+    }
+}
 
 abstract class TemplateClasse
 {
@@ -31,11 +42,20 @@ class Program
 {
     static void Main()
     {
+        // 16.1
+        string[] palavras = { "banana", "uva", "maca", "abacaxi", "kiwi", "pera" };
+        Array.Sort(palavras, new ComparaUltimaLetra());
+
+        Console.WriteLine("Palavras ordenadas pela última letra:");
+        foreach (string palavra in palavras)
+            Console.WriteLine(palavra);
+
+        // 16.2
         GeradorPessoa gerador = new GeradorPessoa();
         gerador.Nome = "Pessoa";
         gerador.Extends = "object";
 
-        Console.WriteLine("Classe gerada:");
+        Console.WriteLine("\nClasse gerada:");
         Console.WriteLine(gerador.Gerar());
     }
 }
